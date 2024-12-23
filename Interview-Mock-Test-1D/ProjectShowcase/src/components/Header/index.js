d17dce30aa8b88c018efbd739200c9f3a7327c0d
ASHICK <rainaashick231173@gmail.com>
2024-12-23 15:53:02 +0000

    2nd Pro

---
+++ Interview-Mock-Test-1D/ProjectShowcase/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import {HeaderContainer, HeaderImage, Own} from './styledComponents'
+
+const Header = () => (
+  <HeaderContainer>
+    <HeaderImage
+      src="https://assets.ccbp.in/frontend/react-js/projects-showcase/website-logo-img.png"
+      alt="website logo"
+    />
+    <Own>Designed by Ashiq</Own>
+  </HeaderContainer>
+)
+
+export default Header
---
+++ Interview-Mock-Test-1D/ProjectShowcase/src/components/Header/styledComponents.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.div`
+  background-color: #f1f5f9;
+  display:flex;
+  flex-direction:row;
+  align-items:center;
+  justify-content:space-between;
+`
+
+export const HeaderImage = styled.img`
+  width: 180px;
+  margin-top: 15px;
+  margin-bottom: 15px;
+  margin-left: 80px;
+`
+export const Own = styled.p`
+  color:#019DFD;
+  font-weight:bold;
+  margin-right:50px;
+`
